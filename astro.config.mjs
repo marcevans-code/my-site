f8f5ca642fbad712dd506254ce305d4a3e1d7af9
Marc Evans <marcevans@Mac-Mini.local>
2026-06-18 09:21:50 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: 'https://powergrabtx.com', // ← this is required
+  integrations: [sitemap()],
 });
